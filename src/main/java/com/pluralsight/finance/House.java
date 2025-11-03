620fad361eebcaccdd2040c929e7c44096b46470
Kylee <kkeith.biz@gmail.com>
2025-11-03 10:28:21 -0500

    House

--- src/main/java/com/pluralsight/finance/House.java
+++ src/main/java/com/pluralsight/finance/House.java
@@ -1,4 +1,17 @@
 package com.pluralsight.finance;
 
-public class House {
+public class House extends FixedAsset {
+    private int yearBuilt,squareFt,bedrooms;
+
+    public House(String name, double marketValue, int yearBuilt, int squareFt, int bedrooms) {
+        super(name, marketValue);
+        this.yearBuilt = yearBuilt;
+        this.squareFt = squareFt;
+        this.bedrooms = bedrooms;
+    }
+    @Override
+    public double getValue(){
+        double bonus = ( bedrooms * 10000 ) + ( squareFt * 50 );
+        return getMarketValue() * bonus;
+    }
 }
