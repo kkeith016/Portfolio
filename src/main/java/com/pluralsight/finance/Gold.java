ed023efd6b811f0176bddfdf7120dad0b8c0a5d0
Kylee <kkeith.biz@gmail.com>
2025-11-03 10:24:15 -0500

    Gold

--- src/main/java/com/pluralsight/finance/Gold.java
+++ src/main/java/com/pluralsight/finance/Gold.java
@@ -1,4 +1,14 @@
 package com.pluralsight.finance;
 
-public class Gold {
+public class Gold extends FixedAsset {
+    private double weight;
+
+    public Gold(String name, double weight, double marketValue) {
+        super(name, marketValue);
+        this.weight = weight;
+    }
+    @Override
+    public double getValue(){
+        return getMarketValue() * weight;
+    }
 }
