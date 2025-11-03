29bef6785ad12a10bac155a8c8a35cf6dd092f44
Kylee <kkeith.biz@gmail.com>
2025-11-03 11:00:49 -0500

    CreditCard

--- src/main/java/com/pluralsight/finance/CreditCard.java
+++ src/main/java/com/pluralsight/finance/CreditCard.java
@@ -1,4 +1,26 @@
 package com.pluralsight.finance;
 
-public class CreditCard {
+public class CreditCard implements Valuable {
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+    public CreditCard(String name, String accountNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void charge(double amount) {
+        if (amount > 0) balance += amount;
+    }
+
+    public void pay(double amount) {
+        if (amount > 0) balance -= amount;
+    }
+
+    @Override
+    public double getValue() {
+        return -balance; // Debt has negative value
+    }
 }
